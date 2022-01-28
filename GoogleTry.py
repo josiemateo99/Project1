146dd30ba24e77ce0cf63b95d89da696137ddda7
josiemateo99 <josiemateo99@gmail.com>
2022-01-27 22:07:25 -0500

    Wrong File Commited

--- GoogleTry.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    try:
-        print("Trying to Connect")
-        sock.connect(('www.google.com', 80))
-        print("Connected!")
-    except:
-        print("Connection Failed")
