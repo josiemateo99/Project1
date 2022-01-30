55fbd281af6a7eac27b2e58d0c9b9eb244c9379d
josiemateo99 <josiemateo99@gmail.com>
2022-01-30 18:11:22 -0500

    trying something new

--- client.py
+++ client.py
@@ -13,7 +13,7 @@ def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 2048)
         try:
-            sock.settimeout(10)
+            sock.settimeout(10000)
         except:
             sys.stderr.write("ERROR: TIMED OUT")
             exit(1)
